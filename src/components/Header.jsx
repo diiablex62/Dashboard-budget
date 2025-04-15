a4c30ad249f3eb000eb990a536d85272107ad688
Aleexj <jalias59@outlook.fr>
2025-04-15 16:22:20 +0200

    installation tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      
+      <h1 className='text-3xl font-bold underline'>Welcome to My Website</h1>
+    </div>
+  )
+}
