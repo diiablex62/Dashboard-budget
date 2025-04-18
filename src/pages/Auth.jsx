f6bb5ec700b538b44db1af3c13b94e2285d68583
Aleexj <jalias59@outlook.fr>
2025-04-18 23:22:07 +0200

    optimisation form

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,28 +1,52 @@
 import React, { useState } from "react";
+import cochonImage from "../assets/img/cochon.png"; 
+
+function InputField({ id, label, type, placeholder }) {
+  return (
+    <div className='mb-4'>
+      <label htmlFor={id} className='block text-sm font-medium text-gray-700'>
+        {label}
+      </label>
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        className='mt-1 w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
+      />
+    </div>
+  );
+}
+
+function SocialButton({ src, alt, text }) {
+  return (
+    <button className='w-full flex items-center justify-center gap-2 border border-gray-300 py-2 rounded-lg hover:bg-gray-100 transition duration-300'>
+      <img src={src} alt={alt} className='w-5 h-5' />
+      {text}
+    </button>
+  );
+}
 
 export default function Auth() {
   const [isLogin, setIsLogin] = useState(true);
 
   return (
     <div className='flex min-h-screen bg-white overflow-hidden relative'>
-      {/* Section gauche ou droite : Image */}
+      {/* Section gauche : Image */}
       <div
-        className={`absolute inset-y-0 ${
-          isLogin ? "left-0 w-3/10" : "right-0 w-3/10"
-        } bg-blue-500 flex items-center justify-center`}
-      >
+        className={`absolute inset-y-0 w-1/2 ${
+          isLogin ? "left-0" : "right-0"
+        } bg-blue-500 flex items-center justify-center`}>
         <img
-          src='https://img.freepik.com/photos-gratuite/vue-dessus-budget-note-ecrite-bloc-notes-stylo-surface-sombre-etudiant-cahier-couleur-argent-gris-college-business_179666-19726.jpg?t=st=1744983182~exp=1744986782~hmac=2208828aa12af4091eb04a066325936c241c46c353846779b181889e5b376401&w=826'
+          src={cochonImage}
           alt='Illustration'
           className='w-full h-screen object-cover'
         />
       </div>
-      {/* Section droite ou gauche : Formulaire */}
+      {/* Section droite : Formulaire */}
       <div
-        className={`absolute inset-y-0 ${
-          isLogin ? "right-0 w-7/10" : "left-0 w-7/10"
-        } flex items-center justify-center bg-white`}
-      >
+        className={`absolute inset-y-0 w-1/2 ${
+          isLogin ? "right-0" : "left-0"
+        } flex items-center justify-center bg-white`}>
         <div className='w-full max-w-md p-8 rounded-lg shadow-md'>
           {isLogin ? (
             <>
@@ -31,38 +55,21 @@ export default function Auth() {
                 Connectez-vous pour continuer
               </p>
               <form>
-                <div className='mb-4'>
-                  <label
-                    htmlFor='email'
-                    className='block text-sm font-medium text-gray-700'
-                  >
-                    Adresse e-mail
-                  </label>
-                  <input
-                    id='email'
-                    type='email'
-                    placeholder='Adresse e-mail'
-                    className='mt-1 w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
-                  />
-                </div>
-                <div className='mb-4'>
-                  <label
-                    htmlFor='password'
-                    className='block text-sm font-medium text-gray-700'
-                  >
-                    Mot de passe
-                  </label>
-                  <input
-                    id='password'
-                    type='password'
-                    placeholder='Mot de passe'
-                    className='mt-1 w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
-                  />
-                </div>
+                <InputField
+                  id='email'
+                  label='Adresse e-mail'
+                  type='email'
+                  placeholder='Adresse e-mail'
+                />
+                <InputField
+                  id='password'
+                  label='Mot de passe'
+                  type='password'
+                  placeholder='Mot de passe'
+                />
                 <button
                   type='submit'
-                  className='w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300'
-                >
+                  className='w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300'>
                   Connexion
                 </button>
               </form>
@@ -72,86 +79,56 @@ export default function Auth() {
                 <div className='flex-1 h-px bg-gray-300'></div>
               </div>
               <div className='space-y-4'>
-                <button className='w-full flex items-center justify-center gap-2 border border-gray-300 py-2 rounded-lg hover:bg-gray-100 transition duration-300'>
-                  <img
-                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png'
-                    alt='Google'
-                    className='w-5 h-5'
-                  />
-                  Connexion avec Google
-                </button>
-                <button className='w-full flex items-center justify-center gap-2 border border-gray-300 py-2 rounded-lg hover:bg-gray-100 transition duration-300'>
-                  <img
-                    src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg'
-                    alt='GitHub'
-                    className='w-5 h-5'
-                  />
-                  Connexion avec GitHub
-                </button>
+                <SocialButton
+                  src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png'
+                  alt='Google'
+                  text='Connexion avec Google'
+                />
+                <SocialButton
+                  src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg'
+                  alt='GitHub'
+                  text='Connexion avec GitHub'
+                />
               </div>
               <p className='text-center text-sm text-gray-600 mt-6'>
                 Vous n'avez pas de compte ?{" "}
                 <button
                   onClick={() => setIsLogin(false)}
-                  className='text-blue-500 hover:underline'
-                >
+                  className='text-blue-500 hover:underline'>
                   Inscrivez-vous ici
                 </button>
               </p>
             </>
           ) : (
             <>
-              <h2 className='text-2xl font-bold text-center mb-2'>Inscription</h2>
+              <h2 className='text-2xl font-bold text-center mb-2'>
+                Inscription
+              </h2>
               <p className='text-center text-sm text-gray-500 mb-6'>
                 Inscrivez-vous pour continuer
               </p>
               <form>
-                <div className='mb-4'>
-                  <label
-                    htmlFor='username'
-                    className='block text-sm font-medium text-gray-700'
-                  >
-                    Nom d'utilisateur
-                  </label>
-                  <input
-                    id='username'
-                    type='text'
-                    placeholder="Nom d'utilisateur"
-                    className='mt-1 w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
-                  />
-                </div>
-                <div className='mb-4'>
-                  <label
-                    htmlFor='email'
-                    className='block text-sm font-medium text-gray-700'
-                  >
-                    Adresse e-mail
-                  </label>
-                  <input
-                    id='email'
-                    type='email'
-                    placeholder='Adresse e-mail'
-                    className='mt-1 w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
-                  />
-                </div>
-                <div className='mb-4'>
-                  <label
-                    htmlFor='password'
-                    className='block text-sm font-medium text-gray-700'
-                  >
-                    Mot de passe
-                  </label>
-                  <input
-                    id='password'
-                    type='password'
-                    placeholder='Mot de passe'
-                    className='mt-1 w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
-                  />
-                </div>
+                <InputField
+                  id='username'
+                  label="Nom d'utilisateur"
+                  type='text'
+                  placeholder="Nom d'utilisateur"
+                />
+                <InputField
+                  id='email'
+                  label='Adresse e-mail'
+                  type='email'
+                  placeholder='Adresse e-mail'
+                />
+                <InputField
+                  id='password'
+                  label='Mot de passe'
+                  type='password'
+                  placeholder='Mot de passe'
+                />
                 <button
                   type='submit'
-                  className='w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300'
-                >
+                  className='w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300'>
                   Inscription
                 </button>
               </form>
@@ -161,29 +138,22 @@ export default function Auth() {
                 <div className='flex-1 h-px bg-gray-300'></div>
               </div>
               <div className='space-y-4'>
-                <button className='w-full flex items-center justify-center gap-2 border border-gray-300 py-2 rounded-lg hover:bg-gray-100 transition duration-300'>
-                  <img
-                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png'
-                    alt='Google'
-                    className='w-5 h-5'
-                  />
-                  Inscription avec Google
-                </button>
-                <button className='w-full flex items-center justify-center gap-2 border border-gray-300 py-2 rounded-lg hover:bg-gray-100 transition duration-300'>
-                  <img
-                    src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg'
-                    alt='GitHub'
-                    className='w-5 h-5'
-                  />
-                  Inscription avec GitHub
-                </button>
+                <SocialButton
+                  src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png'
+                  alt='Google'
+                  text='Inscription avec Google'
+                />
+                <SocialButton
+                  src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg'
+                  alt='GitHub'
+                  text='Inscription avec GitHub'
+                />
               </div>
               <p className='text-center text-sm text-gray-600 mt-6'>
                 Vous avez déjà un compte ?{" "}
                 <button
                   onClick={() => setIsLogin(true)}
-                  className='text-blue-500 hover:underline'
-                >
+                  className='text-blue-500 hover:underline'>
                   Connectez-vous ici
                 </button>
               </p>
