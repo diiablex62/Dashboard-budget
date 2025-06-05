7b46876ca29e35e54f25dc1a94eb846a74edaf5c
Aleexj <jalias59@outlook.fr>
2025-06-05 09:40:27 +0200

    chore: Révision des tooltips dans TooltipBudgetFuture pour améliorer la clarté des informations financières, en reformulant les descriptions et en simplifiant l'affichage des budgets actuels et prévisionnels.

--- src/components/dashboard/tooltips/TooltipBudgetFuture.jsx
+++ src/components/dashboard/tooltips/TooltipBudgetFuture.jsx
@@ -9,22 +9,15 @@ const TooltipBudgetFuture = () => (
   <div className='absolute top-0 right-full mr-2 w-80 max-h-[600px] overflow-y-auto p-3 bg-gray-800 text-white text-xs rounded-lg z-50 shadow-lg'>
     <div>
       <div className='mb-2'>
-        <span className='font-semibold'>Dépenses du 1 du mois jusqu'à aujourd'hui :</span>{" "}
+        <span className='font-semibold'>
+          Budget actuel (du 1 du mois jusqu'à aujourd'hui) :
+        </span>{" "}
         0,00€
       </div>
-      <ul className='mb-2'>
-        <li className='text-red-400'>
-          <span className='font-bold' style={{ color: "#ef4444" }}>
-            Dépenses :
-          </span>{" "}
-          0,00€
-        </li>
-        <li className='text-blue-400'>Paiements récurrents : 0,00€</li>
-        <li className='text-purple-400'>Paiements échelonnés : 0,00€</li>
-      </ul>
+
       <div className='mb-2 mt-4'>
         <span className='font-semibold'>
-          Total prévisionnel à la fin du mois :
+          Budget prévisionnel à la fin du mois :
         </span>{" "}
         0,00€
       </div>
